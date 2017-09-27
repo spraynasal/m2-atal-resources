dd36e8ae659984822ba5a40615b4acafddfddb97
sebastian <sebastian@dictanova.com>
2017-09-27 17:20:40 +0200

    Minimal stub.

---
+++ src/main/java/org/spraynasal/SimplePipelineRunner.java
@@ -0,0 +1,18 @@
+package org.spraynasal;
+
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * @author sebastian
+ */
+public class SimplePipelineRunner {
+
+    public  static void main(String[] args) throws IOException {
+        
+    }
+}
